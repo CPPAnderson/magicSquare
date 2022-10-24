681d88cd3026a14bbfbb926e68770e19703844cf
ChristianA <canderson@cpp.edu>
2022-10-23 22:22:20 -0700

    Start of program. includes include statements, main, and variables needed for program.

---
+++ mSquare.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+
+//global variables needed for program.
+time_t t;
+int num;
+
+
+
+int main(){
+    srand((unsigned) time(&t)); 
+    int array[3][3];
+    int acount;
+    
+    
+
+}
