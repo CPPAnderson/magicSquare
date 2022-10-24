357bec4d73064e5ef1065a5b624f30a28223f3c5
ChristianA <canderson@cpp.edu>
2022-10-23 22:26:07 -0700

    added gameCheck function. makes sure everyting on the double array across, up, down, and diagonal equals 15.

--- mSquare.c
+++ mSquare.c
@@ -11,6 +11,27 @@ int random(){
     return ((rand() % 9) + 1);
 }
 
+// function gameCheck, makes sure everthing across, up, down, diagonal equals 15
+bool gameCheck(int array[3][3]){
+    if((array[0][0] + array [1][0] + array[2][0]) == 15){
+        if((array[0][1] + array [1][1] + array[2][1]) == 15){
+            if((array[0][2] + array [1][2] + array[2][2]) == 15){
+                if((array[0][0] + array [1][1] + array[2][2]) == 15){
+                    if((array[0][2] + array [1][1] + array[2][0]) == 15){
+                        if((array[0][0] + array [0][1] + array[0][2]) == 15){
+                            if((array[1][0] + array [1][1] + array[1][2]) == 15){
+                                if((array[2][0] + array [2][1] + array[2][2]) == 15){
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
 
 
 int main(){
